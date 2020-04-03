3274a39b1c5f22ebd7de8b62d677c7b24471cec8
Paulo Lobo <pauloloboneto@gmail.com>
2020-04-03 19:31:32 -0300

    tratamento de erro modal component not declared

--- src/app/components/modal/modal-body/modal-body.component.ts
+++ src/app/components/modal/modal-body/modal-body.component.ts
@@ -1,6 +1,5 @@
 import { Component, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
-
-
+import { ModalException } from 'src/app/shared/errors/ModalException';
 
 @Component({
     selector: 'app-modal-body-component',
@@ -12,12 +11,19 @@ export class ModalBodyComponent {
 
     closeModal() {
         let elementRef = this.modal.nativeElement;
-        while (elementRef.className != 'modal-component') {
-            if (elementRef.parentNode.className == 'modal-component') {
-                elementRef.parentNode.style.display = 'none';
-                elementRef = elementRef.parentNode;
-            } else {
-                elementRef = elementRef.parentNode;
+        try {
+            while (elementRef.className != 'modal-component') {
+                if (elementRef.parentNode.className == 'modal-component') {
+                    elementRef.parentNode.style.display = 'none';
+                    elementRef = elementRef.parentNode;
+                } else {
+                    elementRef = elementRef.parentNode;
+                }
+            }
+        } catch (error) {
+            const err: TypeError = error;
+            if (err.message == 'Cannot read property \'className\' of null') {
+                throw ModalException.prototype.classNameNotFound();
             }
         }
     }
---
+++ src/app/shared/errors/ModalException.ts
@@ -0,0 +1,6 @@
+export class ModalException{
+
+   classNameNotFound(){
+       return 'É necessário declarar o app-modal-component antes do app-modal-body';
+   }
+}
